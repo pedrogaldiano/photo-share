cff95840f4cd62f72dc0f88b01e6cd340d04a8d5
Pedro Galdiano <pedrogaldiano>
2021-12-02 13:21:38 -0300

    collection ta cheia dos bugs

---
+++ Photo Share/Controllers/Views/Cell.swift
@@ -0,0 +1,13 @@
+//
+//  Cell.swift
+//  Photo Share
+//
+//  Created by PEDRO GALDIANO DE CASTRO on 02/12/21.
+//
+
+import Foundation
+import UIKit
+
+class Cell: UICollectionViewCell {
+    var imageView: UIImageView!
+}
